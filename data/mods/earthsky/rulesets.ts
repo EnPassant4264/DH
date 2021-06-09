88e51823d92b5fc1582f7633e3d6a1347fe40e21
En Passant <enpassant4264@gmail.com>
2021-06-09 00:36:11 -0600

    Earth & Sky debugging
    okay, I knew I should've used uppercase, but you didn't have to crash

--- data/mods/earthsky/rulesets.ts
+++ data/mods/earthsky/rulesets.ts
@@ -23,26 +23,26 @@ export const Formats: {[k: string]: ModdedFormatData} = {
 			for (const set of team) {
 				if (set.moves) {
 					const pokemon = this.dex.getSpecies(set.species || set.name);
-					for (const moveId of set.moves) {
+					for (const moveID of set.moves) {
 						const pokeLearnsMove = this.dex.getLearnsetData(pokemon).learnset[moveID];
-						console.log(pokemon + " knows " + moveId + " with means " + pokeLearnsMove);
+						console.log(pokemon + " knows " + moveID + " with means " + pokeLearnsMove);
 						if(pokeLearnsMove === ["8D"]){
 							console.log("This is a Hidden Move");
 							if(pokemon.restrictedHidden){ //Denotes that Pokemon can't learn alt-forme's or prevo's Hidden Move
 								console.log("This is a restricted Hidden Move");
-								if(pokemon.changesFrom) console.log("Base form is " + pokemon.changesFrom + " and its accessibility to " + moveId + " is " + this.dex.getLearnsetData(this.dex.getSpecies(pokemon.changesFrom)).learnset[moveId]);
+								if(pokemon.changesFrom) console.log("Base form is " + pokemon.changesFrom + " and its accessibility to " + moveID + " is " + this.dex.getLearnsetData(this.dex.getSpecies(pokemon.changesFrom)).learnset[moveID]);
 								if(pokemon.changesFrom && pokemon.name !== pokemon.changesFrom && 
-								this.dex.getLearnsetData(this.dex.getSpecies(pokemon.changesFrom)).learnset[moveId] === ["8D"]) //This move is base forme's Hidden Move
-									problems.push(`${pokemon} can't learn ${moveId} because it is ${pokemon.baseSpecies}'s exclusive Hidden Move.`);
-								if(pokemon.prevo) console.log("Prevo is " + pokemon.prevo.name + " and its accessibility to " + moveId + " is " + this.dex.getLearnsetData(pokemon.prevo).learnset[moveId]);
-								if(pokemon.prevo && this.dex.getLearnsetData(pokemon.prevo).learnset[moveId] === ["8D"]) //This move is prevo's Hidden Move
-									problems.push(`${pokemon} can't learn ${moveId} because it is ${pokemon.prevo}'s exclusive Hidden Move.`);
+								this.dex.getLearnsetData(this.dex.getSpecies(pokemon.changesFrom)).learnset[moveID] === ["8D"]) //This move is base forme's Hidden Move
+									problems.push(`${pokemon} can't learn ${moveID} because it is ${pokemon.baseSpecies}'s exclusive Hidden Move.`);
+								if(pokemon.prevo) console.log("Prevo is " + pokemon.prevo.name + " and its accessibility to " + moveID + " is " + this.dex.getLearnsetData(pokemon.prevo).learnset[moveID]);
+								if(pokemon.prevo && this.dex.getLearnsetData(pokemon.prevo).learnset[moveID] === ["8D"]) //This move is prevo's Hidden Move
+									problems.push(`${pokemon} can't learn ${moveID} because it is ${pokemon.prevo}'s exclusive Hidden Move.`);
 							} else if (pokemon.prevo && pokemon.prevo.restrictedHidden){ //This Pokemon is third-stage, and the second stage can't learn the first stage's Hidden Move
 								const prevo = pokemon.prevo;
 								console.log("Prevo has a restricted Hidden Move");
-								if(prevo.prevo) console.log("First stage is " + prevo.prevo.name + " and its accessibility to " + moveId + " is " + this.dex.getLearnsetData(prevo.prevo).learnset[moveId]);
-								if(prevo.prevo && this.dex.getLearnsetData(prevo.prevo).learnset[moveId] === ["8D"]) //This move is first stage's Hidden Move
-									problems.push(`${pokemon} can't learn ${moveId} because it is ${prevo.prevo}'s exclusive Hidden Move.`);
+								if(prevo.prevo) console.log("First stage is " + prevo.prevo.name + " and its accessibility to " + moveID + " is " + this.dex.getLearnsetData(prevo.prevo).learnset[moveID]);
+								if(prevo.prevo && this.dex.getLearnsetData(prevo.prevo).learnset[moveID] === ["8D"]) //This move is first stage's Hidden Move
+									problems.push(`${pokemon} can't learn ${moveID} because it is ${prevo.prevo}'s exclusive Hidden Move.`);
 							}
 							for(const poke of learnedHiddenTable){
 								if(poke.baseSpecies === pokemon.baseSpecies)
